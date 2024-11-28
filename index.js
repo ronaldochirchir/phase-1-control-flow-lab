93db35d3e14446301453739fc98dab72ac8cff00
Ronaldo Kipkirui <ronaldochirchir8@gmail.com>
2024-11-28 21:46:13 +0300

    completed laps

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someValue) {
+    if (someValue <= 400) {
+      return "This one is on me!";
+    } else if (someValue > 400 && someValue <= 2000) {
+      return "That will be twenty bucks.";
+    } else if (someValue > 2000 && someValue <= 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else {
+      return "No can do.";
+    }
+  }
+  function ternaryCheckCity(city) {
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
+  function switchOnCharmFromTip(tipAmount) {
+    if (tipAmount === "generous") {
+      return "Thank you so much."; 
+    }
+    else if (tipAmount === "not as generous") {
+      return "Thank you.";
+    }
+    else {
+      return "Bye.";
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
